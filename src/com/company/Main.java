ee6ac048b623c94e1310d4ecb6008d384adabcd4
vlladd <E11ori0n@bk.ru>
2017-11-03 18:13:12 +1000

    newWarcK

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+
+
+
+        public static void main(String[] args) {
+            int Pety = 10;
+            int Eliss = 5;
+            System.out.println(Pety + Eliss);
+        }
+
+    }
+
